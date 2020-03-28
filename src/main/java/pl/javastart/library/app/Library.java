40fd8291fded1763393fbfa901c549203eccad48
reszka303 <reszka303@op.pl>
2020-03-28 21:13:08 +0100

    Library v0.7

--- src/main/java/pl/javastart/library/app/Library.java
+++ src/main/java/pl/javastart/library/app/Library.java
@@ -4,18 +4,19 @@ import pl.javastart.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Library v0.6";
+        final String appName = "Library v0.7";
 
-        Book book1 = new Book("In Desert and Wilderness", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
-        Book book2 = new Book("Effective Java. Release II", "Joshua Bloch", 2009, 352, "Helion",
+        Book[] books = new Book[1000];
+        books[0] = new Book("In Desert and Wilderness", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
+        books[1] = new Book("Effective Java. Release II", "Joshua Bloch", 2009, 352, "Helion",
                 "9788324620845");
-        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
                 2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Books available in the library:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
     }
 }

45dea01a5788ebded18b4d2a80e1f2e71bd4faa1
reszka303 <reszka303@op.pl>
2020-05-31 00:36:21 +0200

    Library v1.3

--- src/main/java/pl/javastart/library/model/Book.java
+++ src/main/java/pl/javastart/library/model/Book.java
@@ -1,5 +1,7 @@
 package pl.javastart.library.model;
 
+import java.util.Objects;
+
 public class Book extends Publication {
     private String author;
     private int pages;
@@ -13,7 +15,6 @@ public class Book extends Publication {
         this.isbn = isbn;
     }
 
-    // settery i gettery
     public String getIsbn() {
         return isbn;
     }
@@ -38,10 +39,25 @@ public class Book extends Publication {
         this.author = author;
     }
 
+
+        @Override
+    public String toString() {
+        return super.toString() + ", " + author + ", " + pages + ", " + isbn;
+    }
+
     @Override
-    public void printInfo() {
-        String info = getTitle() + "; " + author + "; " + getYear() + "; "
-                + pages + "; " + getPublisher() + "; " + isbn;
-        System.out.println(info);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return pages == book.pages &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author, pages, isbn);
     }
 }
--- src/main/java/pl/javastart/library/model/Magazine.java
+++ src/main/java/pl/javastart/library/model/Magazine.java
@@ -1,5 +1,7 @@
 package pl.javastart.library.model;
 
+import java.util.Objects;
+
 public class Magazine extends Publication {
     private int month;
     private int day;
@@ -32,9 +34,23 @@ public class Magazine extends Publication {
     }
 
     @Override
-    public void printInfo() {
-        String info = getTitle() + "; " + getPublisher() + "; " + getYear() + "-"
-                + month + "-" + day + "; " + language;
-        System.out.println(info);
+    public String toString() {
+        return super.toString() + ", " + month + ", " + day + ", " + language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Magazine magazine = (Magazine) o;
+        return month == magazine.month &&
+                day == magazine.day &&
+                Objects.equals(language, magazine.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), month, day, language);
     }
 }

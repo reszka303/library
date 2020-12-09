efe72e8e91b6225d6f114eba63af8531081f68b7
reszka303 <reszka303@op.pl>
2020-12-09 02:06:37 +0100

    Biblioteka v1.9

--- src/main/java/pl/javastart/library/io/file/FileManager.java
+++ src/main/java/pl/javastart/library/io/file/FileManager.java
@@ -3,6 +3,6 @@ package pl.javastart.library.io.file;
 import pl.javastart.library.model.Library;
 
 public interface FileManager {
-    Library importData();
-    void exportData(Library library);
+    Library importData();//read
+    void exportData(Library library);//write
 }

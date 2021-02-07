8c1c178fb2e3fce693db0b509948820273f46fb2
reszka303 <reszka303@op.pl>
2021-02-07 21:40:50 +0100

    Biblioteka v2.4.1

--- src/main/java/pl/javastart/library/app/LibraryApp.java
+++ src/main/java/pl/javastart/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v2.4";
+    private static final String APP_NAME = "Biblioteka v2.4.1";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
